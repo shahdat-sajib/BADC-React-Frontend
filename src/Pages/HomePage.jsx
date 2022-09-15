c2d9e32b408bb195bf79c02f25f35596e80efe7c
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2022-09-15 15:02:52 +0600

    appbar component ready

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomePage from '../Components/Appbar';
+
+const DemoPage = () => {
+    return (
+        <div>
+            <HomePage/>
+            <h2>Homepage</h2>
+        </div>
+    );
+}
+
+export default DemoPage;
