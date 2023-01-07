e077c85da2765fbbb78e03e89c4175c92df0c726
shahdat-sajib <shahdat35-2757@diu.edu.bd>
2023-01-08 00:37:10 +0600

    phonelist show , update and delete functionality working

--- src/Components/phonelist.js
+++ src/Components/phonelist.js
@@ -1,17 +1,51 @@
 import React from "react";
 import { CheckCircle } from "@mui/icons-material";
 import { Delete } from "@mui/icons-material";
+import { Edit } from "@mui/icons-material";
 export default function Phonelist({
-    phone,
+    todo,
     toggleComplete,
     handleDelete,
     handleEdit
 }) {
-    // const [newTitle, setNewTitle] = React.useState(phone.title);
+    const [newTitle, setNewTitle] = React.useState(todo.title);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        if (todo.complete === true) {
+          setNewTitle(todo.title);
+        } else {
+          todo.title = "";
+          setNewTitle(e.target.value);
+        }
+      };
 
     return (
         <div>
-            <h3>Im list</h3>
+            <input
+            style={{textDecoration: todo.completed && "line-through"}}
+            type="text"
+            value={todo.title === "" ? newTitle : todo.title}
+            className="list"
+            onChange={handleChange}
+            />
+            <div>
+        <button
+          className="button-complete"
+          onClick={() => toggleComplete(todo)}
+        >
+          <CheckCircle id="i" />
+        </button>
+        <button
+          className="button-edit"
+          onClick={() => handleEdit(todo, newTitle)}
+        >
+          <Edit id="i" />
+        </button>
+        <button className="button-delete" onClick={() => handleDelete(todo.id)}>
+          <Delete id="i" />
+        </button>
+      </div>
         </div>
     )
 }
--- src/Pages/SechGrahok.jsx
+++ src/Pages/SechGrahok.jsx
@@ -1,53 +1,63 @@
-// import React from 'react'
-// import Appbar from '../Components/Appbar';
-
-// const SechGrahok = () => {
-//   return (
-//     <div>
-//       <Appbar />
-//       <div>
-//         Sech Grahok
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default SechGrahok;
-
 import React from 'react';
 import Appbar from '../Components/Appbar';
 import { db } from '../firebase';
 import { collection, addDoc } from 'firebase/firestore';
+import { query, onSnapshot, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import Phonelist from '../Components/phonelist';
 
 const SechGrahok = () => {
-  const [name, setName] = React.useState("");
+  const [title, setTitle] = React.useState("");
   const [phone, setPhone] = React.useState("");
   const [type, setType] = React.useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (name !== "" && phone !== "" && type !== "") {
+    if (title !== "" && phone !== "" && type !== "") {
       await addDoc(collection(db, "todos"), {
-        name,
+        title,
         phone,
         type,
       });
-      setName("");
+      setTitle("");
       setPhone("");
       setType("");
     }
   }
+
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+  
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+
 
   return (
     <div style={{ backgroundColor: "white" }}>
       <div>
         <Appbar />
         <Form onSubmit={handleSubmit}>
-          <TextField className='mb-2 mt-3' size='small' id="name" label="Name" variant="outlined" value={name} onChange={(e) => setName(e.target.value)} />
+          <TextField className='mb-2 mt-3' size='small' id="title" label="Name" variant="outlined" value={title} onChange={(e) => setTitle(e.target.value)} />
           <TextField className='mb-2' id="phone" size='small' label="Phone" variant="outlined" value={phone} onChange={(e) => setPhone(e.target.value)} />
           <FormControl className='mb-2' style={{width: "220px"}}>
             <InputLabel size='small' id="dropdown">Type</InputLabel>
@@ -66,7 +76,15 @@ const SechGrahok = () => {
             <Button style={{ backgroundColor: "green" }} className='px-5 py-1' type='submit'>Add</Button>
           </div>
           <div>
-            <Phonelist/>
+          {todos.map((todo) => (
+          <Phonelist
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        ))}
           </div>
         </Form>
       </div>
